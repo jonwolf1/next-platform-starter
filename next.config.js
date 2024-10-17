258aef93ffbc2470668f0e611c6ccf31b70d601a
jonwolf1 <jonathan@wolfgestao.com>
2024-10-17 08:08:09 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/auth/callback',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
-
-module.exports = nextConfig;
